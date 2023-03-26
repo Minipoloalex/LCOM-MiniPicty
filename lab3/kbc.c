ffc4849b0cdc2cdb88f464ea580d0ac3f3d6d9ad
Félix Martins <felix.marcial@hotmail.com>
2023-03-26 12:31:47 +0100

    Added useful comment

--- lab3/kbc.c
+++ lab3/kbc.c
@@ -19,11 +19,14 @@ int (read_kbc_output)(int port, uint8_t *output) {
   return 1;
 }
 
+// Write to kbc a command (0x64), or arguments to a command (0x60)
 int (write_kbc_command)(int port, uint8_t command) {
   uint8_t status;
   for (int i = 0; i < MAX_ATTEMPTS; i++) {
     if (read_kbc_status(&status)) return 1;
-    if ((status & KBC_IBF) == 0) {  // can write command to KBC
+    
+    // IBF = 1 means don't write commands or arguments (0x60 or 0x64)
+    if ((status & KBC_IBF) == 0) {
       if (sys_outb(port, command)) return 1;
       return 0;
     }
