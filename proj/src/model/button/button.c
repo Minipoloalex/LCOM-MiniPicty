b23f8375aeb300f43968982ccfed57f772431043
Félix Martins <felix.marcial@hotmail.com>
2023-05-24 16:12:58 +0100

    Comments and prints removed

--- proj/src/model/button/button.c
+++ proj/src/model/button/button.c
@@ -23,22 +23,18 @@ int (get_hovered_button)(button_t *buttons, int num_buttons, position_t mouse_po
 int (process_buttons_clicks)(button_t *buttons, int num_buttons, drawing_position_t before, drawing_position_t next, int *button_clicked) {
   static int pressed_button = -1;
   static int button_hovered = -1;
-  printf("values of - pressed_button: %d, button_hovered: %d\n", pressed_button, button_hovered);
   button_hovered = get_hovered_button(buttons, num_buttons, next.position);
   if (pressed_button != -1 && pressed_button == button_hovered
-  && !next.is_drawing && before.is_drawing) {
-    printf("clicking on button %d\n", pressed_button);
+&& !next.is_drawing && before.is_drawing) {
     *button_clicked = pressed_button;
     pressed_button = -1;
     return EXIT_SUCCESS;
   }
   else if (button_hovered != -1 && next.is_drawing && !before.is_drawing) {
-    printf("pressed button %d\n", button_hovered);
     pressed_button = button_hovered;
   }
   else if (before.is_drawing && !next.is_drawing && pressed_button != -1 
   && button_hovered != pressed_button) {
-    printf("releasing button %d\n", pressed_button);
     pressed_button = -1;
   }
   return EXIT_SUCCESS;
--- proj/src/modules/game/game.c
+++ proj/src/modules/game/game.c
@@ -152,27 +152,6 @@ int (game_process_mouse)() {
       return EXIT_FAILURE;
     }
   }
-  // printf("processing mouse\n");
-  // static int pressed_button = -1;
-  // static int button_hovered = -1;
-  // printf("values of - pressed_button: %d, button_hovered: %d\n", pressed_button, button_hovered);
-  // button_hovered = is_cursor_over_game_button(next.position);
-  // if (pressed_button != -1 && pressed_button == button_hovered 
-  // && !next.is_drawing && before.is_drawing) {
-  //   printf("clicking on button %d\n", pressed_button);
-  //   game_buttons[button_hovered].onClick(&game_buttons[button_hovered]);
-  //   ser_add_button_click_to_transmitter_queue(button_hovered);
-  //   pressed_button = -1;
-  // }
-  // else if (button_hovered != -1 && next.is_drawing && !before.is_drawing) {
-  //   printf("pressed button %d\n", button_hovered);
-  //   pressed_button = button_hovered;
-  // }
-  // else if (before.is_drawing && !next.is_drawing && pressed_button != -1 
-  // && button_hovered != pressed_button) {
-  //   printf("releasing button %d\n", pressed_button);
-  //   pressed_button = -1;
-  // }
   int button_to_click = -1;
   if (process_buttons_clicks(game_buttons, NUMBER_GAME_BUTTONS, before, next, &button_to_click)) {
     printf("process_buttons_clicks inside %s\n", __func__);
